da8611eb870c5cc71039b47a165bce10e63ef563
schismatiic <leonardojofre2003@gmail.com>
2026-09-06 14:17:31 -0300

    Quick fix

--- controllers/membershipController.js
+++ controllers/membershipController.js
@@ -1,8 +1,6 @@
 const db = require("../db/queries");
 const { body, validationResult, matchedData } = require("express-validator");
 
-process.loadEnvFile();
-
 const validateBecome = [
   body("password")
     .trim()
