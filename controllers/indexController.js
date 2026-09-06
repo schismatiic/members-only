6d512ebe5092effb2da029c0c2a5e1225b84842c
schismatiic <leonardojofre2003@gmail.com>
2026-09-06 13:08:24 -0300

    Date added

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,7 +2,21 @@ const db = require("../db/queries");
 
 const getMessages = async (req, res) => {
   const messages = await db.getAllMessages();
-  res.render("index", { messages, user: req.user });
+  const formattedMessages = messages.map((message) => ({
+    ...message,
+    added: message.added
+      .toLocaleString("en-GB", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      })
+      .replace(/\//g, "/")
+      .replace(", ", " - "),
+  }));
+  res.render("index", { messages: formattedMessages, user: req.user });
 };
 
 module.exports = { getMessages };
